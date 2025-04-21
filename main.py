5fdb039c0a45b41eb53e813e339f89a4981939a0
Gabriele Soldati <soldati.gabriele@gmail.com>
2025-04-21 13:50:20 +0100

    System

--- main.py
+++ main.py
@@ -9,13 +9,20 @@ import numpy as np
 
 load_dotenv()
 TOKEN = os.getenv("PORCUPINE_TOKEN")
+SYSTEM = os.getenv("SYSTEM")
+if SYSTEM == "mac":
+    porcupine = pvporcupine.create(
+        keywords=["hey lampo"], 
+        access_key=TOKEN,
+        keyword_paths=['mac/hey-lampo_it.ppn'],
+        model_path='porcupine_params_it.pv',
+    )
+else:
+    porcupine = pvporcupine.create(
+        keywords=["blueberry"], 
+        access_key=TOKEN,
+    )
 
-porcupine = pvporcupine.create(
-    keywords=["hey lampo"], 
-    access_key=TOKEN,
-    keyword_paths=['mac/hey-lampo_it.ppn'],
-    model_path='porcupine_params_it.pv',
-)
 
 pa = pyaudio.PyAudio()
 stream = pa.open(rate=porcupine.sample_rate,
